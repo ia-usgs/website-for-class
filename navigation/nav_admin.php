fd426632dbea52a5df0914562ac61c44acf141c6
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:20:38 -0700

    Update nav_admin.php

--- navigation/nav_admin.php
+++ navigation/nav_admin.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="../functions/logout.php">Logout</a></li>
         <li><a href="../functions/manage_vehicles.php">Manage Vehicles</a></li>
-        <li><a href="../add/add_make.php">Add Make</a></li>
+        <li><a href="../functions/manage_users.php">Manage Users</a></li>
         <li><a href="../pages/admin_page.php">Admin Dashboard</a></li>
     </ul>
 </nav>
