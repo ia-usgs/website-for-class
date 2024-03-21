27e0eb708a29c4ec512eb7e731350a0505d3ee90
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:26:00 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials
-$host = 'localhost'; // or your database host, e.g., a remote server
-$db_user = 'root'; // your database username
-$db_password = 'kali'; // your database password
-$db_name = 'used_car_database'; // your database name
+$host = 'localhost'; // database host
+$db_user = 'root'; // database username
+$db_password = ''; // database password
+$db_name = 'used_car_database'; // database name
 
 // Create a new database connection instance
 $conn = new mysqli($host, $db_user, $db_password, $db_name);
@@ -13,8 +13,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// If you wish to set the charset (optional)
 $conn->set_charset("utf8");
 
-// The connection is now established and you can use $conn to interact with your database
 ?>
