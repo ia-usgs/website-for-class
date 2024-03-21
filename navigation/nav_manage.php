37d056e95f3cf954b4db369fa290297c61642239
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:21:01 -0700

    Update nav_manage.php

--- navigation/nav_manage.php
+++ navigation/nav_manage.php
@@ -1,7 +1,7 @@
 <nav class="sidebar">
     <ul>
         <li><a href="../functions/logout.php">Logout</a></li>
-        <li><a href="../functions/new_user.php">View Vehicles</a></li>
-        <li><a href="../functions/admin_page.php">Profile</a></li>
+        <li><a href="../add/add_user.php">New User</a></li>
+        <li><a href="../pages/admin_page.php">Admin Page</a></li>
     </ul>
 </nav>
