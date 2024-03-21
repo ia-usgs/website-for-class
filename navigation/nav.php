666efeacecdef69e7151ee4a4e9681a9f440f4e1
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:20:02 -0700

    Update nav.php

--- navigation/nav.php
+++ navigation/nav.php
@@ -1,9 +1,8 @@
-<!-- nav.php -->
 <nav class="sidebar">
     <ul>
         <li><a href="../functions/logout.php">Logout</a></li>
-        <li><a href="../functions/new_make.php">New Make</a></li> <!-- Adjust as needed for different pages -->
+        <li><a href="../add/add_make.php">New Make</a></li> 
         <li><a href="../functions/manage_vehicles.php">Manage Vehicles</a></li>
-        <li><a href="../functions/admin_page.php">Admin Page</a></li>
+        <li><a href="../pages/admin_page.php">Admin Page</a></li>
     </ul>
 </nav>
