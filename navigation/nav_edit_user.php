b49c5a40e3e5d1b7f55cfbc5bd9274f6382aafb0
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:22:19 -0700

    Create nav_edit_user.php

---
+++ navigation/nav_edit_user.php
@@ -0,0 +1,7 @@
+<nav class="sidebar">
+    <ul>
+        <li><a href="../functions/logout.php">Logout</a></li>
+        <li><a href="../functions/manage_users.php">Manage Users</a></li>
+        <li><a href="../pages/admin_page.php">Admin Page</a></li>
+    </ul>
+</nav>
