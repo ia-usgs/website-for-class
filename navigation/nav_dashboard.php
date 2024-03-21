09bc091a0720f52e5fe5af97d7fc794295d18bc2
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-21 08:20:49 -0700

    Update nav_dashboard.php

--- navigation/nav_dashboard.php
+++ navigation/nav_dashboard.php
@@ -1,8 +1,7 @@
 <nav class="sidebar">
     <ul>
         <li><a href="../functions/logout.php">Logout</a></li>
-        <li><a href="../functions/manage_users.php">Manage Users</a></li>
-        <li><a href="../functions/manage_vehicles.php">Manage Vehicles</a></li>
-        <li><a href="../functions/inventory.php">Manage Inventory</a></li>
+        <li><a href="../functions/see_users.php">Users</a></li>
+        <li><a href="../functions/display_vehicles.php">Inventory</a></li>
     </ul>
 </nav>
