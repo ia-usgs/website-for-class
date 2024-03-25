7b67eda220f3fb15f14f0ee6c8bdb30db87cbf5f
Irvin Avitia <97401201+ia-usgs@users.noreply.github.com>
2024-03-25 09:43:06 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 // Database credentials
-$host = '10.109.250.223'; // database host
+$host = '172.30.58.67'; // database host
 $db_user = 'root'; // database username
 $db_password = 'kali'; // database password
 $db_name = 'used_car_database'; // database name
